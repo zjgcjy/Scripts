fcb406c636734d64722a0fb422534810a9de1330
zjgcjy <zjgcjy@gmail.com>
2022-09-12 16:34:30 +0800

    fix dvrt_parser error on win11(10.0.22621)

--- dvrt_parser.py
+++ dvrt_parser.py
@@ -3,6 +3,18 @@ __author__ = "zjgcjy"
 import binascii
 import ctypes
 import pefile
+import sys
+
+# see ntimage.h 22621
+IMAGE_DYNAMIC_RELOCATION_GUARD_RF_PROLOGUE             = 0x00000001
+IMAGE_DYNAMIC_RELOCATION_GUARD_RF_EPILOGUE             = 0x00000002
+IMAGE_DYNAMIC_RELOCATION_GUARD_IMPORT_CONTROL_TRANSFER = 0x00000003
+IMAGE_DYNAMIC_RELOCATION_GUARD_INDIR_CONTROL_TRANSFER  = 0x00000004
+IMAGE_DYNAMIC_RELOCATION_GUARD_SWITCHTABLE_BRANCH      = 0x00000005
+IMAGE_DYNAMIC_RELOCATION_ARM64X                        = 0x00000006
+IMAGE_DYNAMIC_RELOCATION_FUNCTION_OVERRIDE             = 0x00000007
+IMAGE_DYNAMIC_RELOCATION_MM_SHARED_USER_DATA_VA        = 0x7FFE0000
+#IMAGE_DYNAMIC_RELOCATION_KI_USER_SHARED_DATA64        = 0xFFFFF78000000000UI64
 
 class dvrt():
     def __init__(self, driver, path_prefix = 'C:/Windows/System32/drivers/'):
@@ -25,10 +37,9 @@ class dvrt():
             while self.get_dvrt_foa() not in [0x00, 0x01]:
                 pass
             version = self.read_dword()
-            print('version: 0x%04x' % version)
             size = self.read_dword()
-            print('size: 0x%08x\n' % size)
             end = self.ptr + size
+            print(f'version: 0x{version:02x}, size: 0x{size:08x}, end: 0x{end:08x}')
             while self.ptr < end:
                 self.parse_dvrt()
 
@@ -57,38 +68,45 @@ class dvrt():
             self.ptr -= 4
             return VirtualAddress
         SizeOfBlock = self.read_dword()
-        #print('VirtualAddress: 0x%08x, SizeOfBlock: 0x%08x'% (VirtualAddress, SizeOfBlock))
+        # print(f'VirtualAddress: 0x{VirtualAddress:08x}, SizeOfBlock: 0x{SizeOfBlock:08x}')
         self.ptr += SizeOfBlock - 8
         return VirtualAddress
 
     def parse_dvrt(self):
-        print('ptr: %08x'%self.ptr)
         Symbol = self.read_qword()
         BaseRelocSize = self.read_dword()
-        print('Symbol: 0x%04x, BaseRelocSize: 0x%08x' % (Symbol, BaseRelocSize))
-        stop = self.ptr - 8 + BaseRelocSize
+        print(f'ptr: 0x{self.ptr:08x}, Symbol: 0x{Symbol:02x}, BaseRelocSize: 0x{BaseRelocSize:08x}')
+        stop = self.ptr + BaseRelocSize
+        # TODO: win11 ntoskrnl!RtlPerformRetpolineRelocationsOnImageEx
+        if Symbol == IMAGE_DYNAMIC_RELOCATION_FUNCTION_OVERRIDE:
+            self.ptr = stop
+            return
         while self.ptr < stop:
             VirtualAddress = self.read_dword()
-            # print('VirtualAddress: 0x%08x' % VirtualAddress)
+            # print(f'VirtualAddress: 0x{VirtualAddress:08x})
             SizeOfBlock = self.read_dword()
-            # print('SizeOfBlock: 0x%08x' % SizeOfBlock)
+            # print(f'SizeOfBlock: 0x{SizeOfBlock:08x})
             for i in range((SizeOfBlock - 8) // 4):
                 item = self.read_dword()
-                if Symbol == 3:
+                if Symbol == IMAGE_DYNAMIC_RELOCATION_GUARD_IMPORT_CONTROL_TRANSFER:
                     va = VirtualAddress + (item & 0xfff)
                     self.rva_list.append((va, 12))
-                elif Symbol == 4:
+                elif Symbol == IMAGE_DYNAMIC_RELOCATION_GUARD_INDIR_CONTROL_TRANSFER:
                     va = VirtualAddress + (item & 0xfff)
                     if (item >> 12) & 0x01:
                         self.rva_list.append((va, 6))
                     else:
                         self.rva_list.append((va, 5))
-                elif Symbol == 5:
+                elif Symbol == IMAGE_DYNAMIC_RELOCATION_GUARD_SWITCHTABLE_BRANCH:
                     va = VirtualAddress + (item & 0xfff)
                     self.rva_list.append((va, 5))
+                elif Symbol == IMAGE_DYNAMIC_RELOCATION_ARM64X:
+                    pass
+                elif Symbol == IMAGE_DYNAMIC_RELOCATION_FUNCTION_OVERRIDE:
+                    pass
                 else:
                     assert(1 == 0)
-                    exit()
+                    sys.exit()
 
 def main():
     obj = dvrt('dxgkrnl.sys', './')
